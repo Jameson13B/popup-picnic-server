0e1f2728c348950f1b1cd026a925d1deb4527a42
Jameson13B <jamesonbrown@neolivity.com>
2019-03-21 16:03:51 -0600

    Updated cors configurations

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(bodyParser.json());
 // Enable CORS for all HTTP methods
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, PATCH, POST, DELETE, OPTIONS"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
